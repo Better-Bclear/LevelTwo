add77a255ad8ae69a6411105be6a408de8f76085
jin1357908642@163.com <jin1357908642@163.com>
2023-07-06 11:21:24 +0800

    Mybatis测试连接

---
+++ Mybatis_Day01/src/test/java/com/baizhi/jinzhanqing/util/UtilTest.java
@@ -0,0 +1,27 @@
+package com.baizhi.jinzhanqing.util;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class UtilTest {
+    @Test
+    public void testConnection(){
+        try {
+            InputStream resourceAsStream = Resources.getResourceAsStream("config/mybatis-config.xml");
+            SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+            SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(resourceAsStream);
+            SqlSession sqlSession = sqlSessionFactory.openSession();
+            System.out.println(sqlSession.getConnection());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
